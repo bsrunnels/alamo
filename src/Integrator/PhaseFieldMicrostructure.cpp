57e2fc00a71375e91154e525f9648159a8931e8b
Mahi Gokuli <mgokuli@caltech.edu>
2019-07-29 11:12:17 -0600

    isotropic replacement w anistropy on whoops
    Former-commit-id: 3b8581066e729479b6f4fbbf2d0fc683e2acc7c5

--- src/Integrator/PhaseFieldMicrostructure.cpp
+++ src/Integrator/PhaseFieldMicrostructure.cpp
@@ -352,13 +352,13 @@ PhaseFieldMicrostructure::Advance (int lev, amrex::Real time, amrex::Real dt)
 			
 			
  							//etanew(i,j,k,m) = eta(i,j,k,m) - M*dt*(W - (Boundary_term) + beta*(Curvature_term));
-							 /* etanew(i,j,k,m) =
+							 etanew(i,j,k,m) =
  								eta(i,j,k,m) -
  								M*dt*(mu*(eta(i,j,k,m)*eta(i,j,k,m) - 1.0 + 2.0*gamma*sum_of_squares)*eta(i,j,k,m)
- 								      - kappa*laplacian); //isotropic resp */
-							etanew(i,j,k,m) = eta(i,j,k,m) - 
+ 								      - kappa*laplacian); //isotropic resp 
+							/* etanew(i,j,k,m) = eta(i,j,k,m) - 
 								M*dt*((Mu*(eta(i,j,k,m)*eta(i,j,k,m) - 1.0 + 2.0*gamma*sum_of_squares)*eta(i,j,k,m)) - 
-									(Boundary_term) + beta*(Curvature_term));
+									(Boundary_term) + beta*(Curvature_term)); */
 							if (std::isnan(etanew(i,j,k,m))) Util::Abort(INFO,"nan at m=",i,",",j,",",k);
 
 						// mahi - remove later
